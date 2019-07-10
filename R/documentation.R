5870c84804637b475026368c041cf664b8bc25cc
Jo Watts <jo@effini.com>
2019-07-10 09:13:06 +0100

    Added additional datasets for the visualisation week

--- R/documentation.R
+++ R/documentation.R
@@ -221,4 +221,146 @@ NULL
 #' @format A data frame
 "bayestown_survey"
 
+#' QikBit_RevBreakdown
+#'
+#' 2019 Sales by product group for (fictional company) QikBit
+#'
+#' @format A data frame
+"Q1data"
+
+#' QikBit_MonthlyFigs
+#'
+#' Aug 2018 - Jul 2019 revenue and costs for QikBit, plus sales for single product line
+#'
+#' @format A data frame
+"Q23data"
+
+#' QikBit_Competitors
+#'
+#' 2015-2019 Revenue for QikBit's competitors
+#'
+#' @format A data frame
+"Q4data"
+
+#' accData
+#'
+#' Comparative accelerometer data for 4 devices and 5 individuals over a single day
+#'
+#' @format A data frame
+"Q4data"
+
+#' bank_expense
+#'
+#' Selected items from large bank's profit/loss statement
+#'
+#' @format A data frame
+"bank_expense"
+
+#' competencies
+#'
+#' Assessment of 4 widget companies' core competencies
+#'
+#' @format A data frame
+"competencies"
+
+#' EUbank
+#'
+#' Banking crises in European countries 1800-2010
+#'
+#' @format A data frame
+"EUbank"
+
+#' euro_ineq
+#'
+#' Volatility of GDP, Gini coefficient (measure of inequality) and percentage
+#' of GDP attributable to services for 17 European countries.
+#'
+#' @format A data frame
+"euro_ineq"
+
+#' inflation4
+#'
+#' Inflation (CPI) for UK, France, Germany and Spain 1960-2010
+#'
+#' @format A data frame
+"inflation4"
+
+#' invest_alluvial
+#'
+#' Lifestyled pension investment allocations ages 40,50,60 - alluvial format
+#'
+#' @format A data frame
+"invest_alluvial"
+
+#' invest_lodes
+#'
+#' Lifestyled pension investment allocations ages 40,50,60 - lodes format
+#'
+#' @format A data frame
+"invest_lodes"
+
+#' invest_lodes2
+#'
+#' Lifestyled pension investment allocations ages 40,50,60 -
+#' lodes format with outgoings
+#'
+#' @format A data frame
+"invest_lodes2"
+
+#' late_deliveries
+#'
+#' Number of late deliveries over 12 calendar months for fictional company
+#'
+#' @format A data frame
+"late_deliveries"
+
+#' milk
+#'
+#' Composition of mammal's milk
+#'
+#' @format A data frame
+"milk"
+
+#' scot_exports_2017
+#'
+#' Scottish exports by sector (and sub-sector) in 2017
+#'
+#' @format A data frame
+"scot_exports_2017"
+
+#' stonybridge
+#'
+#' Words from Stonybridge promotion
+#'
+#' @format A data frame
+"stonybridge"
+
+#' VoteEU
+#'
+#' Two way table - vote in 2016 EU referendum and 2019 voting intention
+#' Converted to ggplot2 layout
+#'
+#' @format A data frame
+"vote19_eu"
+
+#' death_note
+#'
+#' Main causes of death in UK males in 2016
+#'
+#' @format A data frame
+"death_males"
+
+#' flatPrices
+#'
+#' Estimated average flat prices and monthly rents in Edinburgh 2011-2019
+#'
+#' @format A data frame
+"flatPrices"
+
+#' world
+#'
+#' World map combined with data from Global Attitudes Survey 2017
+#'
+#' @format A data frame
+"world"
 
